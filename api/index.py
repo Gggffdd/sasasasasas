900682c0947f08471edeca55fa6b617d85c71406
Gggffdd <165726917+Gggffdd@users.noreply.github.com>
2025-11-03 02:14:42 +0300

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "NOTLIFE API"}
+
+@app.get("/api/health")
+async def health():
+    return {"status": "healthy"}
